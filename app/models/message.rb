f1b003f59f81e1a84d5d91a3de990938cd0b91fd
Stephen Crespo <screspo988@gmail.com>
2016-04-18 15:33:01 -0400

    New Chat

---
+++ app/models/message.rb
@@ -0,0 +1,12 @@
+class Message < ApplicationRecord
+  after_create_commit :perform
+
+  def perform
+    ActionCable.server.broadcast 'room_channel', message: render_message
+  end
+
+  private
+    def render_message
+      ApplicationController.renderer.render(partial: 'messages/message', locals: { message: self })
+    end
+end
